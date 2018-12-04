7d97d04f2c672251aa2a26227144f1f4999a66c5
rayvayvo <rayvayvo@gmail.com>
2018-12-04 17:49:11 -0600

    Revert "changed griddata layout"
    This reverts commit 9b77efcc52fb84b09763727a3cf5678dbd814101.

--- Assets/Scripts/Tiler.cs
+++ Assets/Scripts/Tiler.cs
@@ -1,4 +1,4 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,58 +18,6 @@ public class Tiler : MonoBehaviour {
             {
                 GridData[iX, iY] = 0;
                 Instantiate(GridTile, new Vector3(iX, 0, iY), Quaternion.identity);                
-            }
-        }
-    }
-
-}*/
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tiler : MonoBehaviour
-{
-
-    public string contents;
-    public int[] faceS = new int[3];
-    public int[] faceE;
-    public int[] faceN;
-    public int[] faceW;
-    public int[] faceT;
-    public int[] faceB;
-    public string attachedObject;
-    public int[] objectTransform;
-
-    public GameObject GridTile;
-    public GameObject CanvasDb;
-    public Tiler[,] gridData;
-
-    public void Start()
-    {
-        gridData = new Tiler[50, 50];
-
-        for (int iX = 0; iX < 50; iX++)
-        {
-            for (int iY = 0; iY < 50; iY++)
-            {
-                gridData[iX, iY] = new Tiler();
-                gridData[iX, iY].contents = "Empty";
-                gridData[iX, iY].attachedObject = "Empty";
-
-                for (int iZ = 0; iZ <= 3; iZ++)
-                {
-                    gridData[iX, iY].faceS[iZ] = 0;
-                    gridData[iX, iY].faceE[iZ] = 0;
-                    gridData[iX, iY].faceW[iZ] = 0;
-                    gridData[iX, iY].faceN[iZ] = 0;
-                    gridData[iX, iY].faceT[iZ] = 0;
-                    gridData[iX, iY].faceB[iZ] = 0;
-                    gridData[iX, iY].objectTransform[iZ] = 0;
-                }
-
-
-                Instantiate(GridTile, new Vector3(iX, 0, iY), Quaternion.identity);
             }
         }
     }
